827d283f028b95c6f8504c3be40dac0b1cad4b69
tiago <tiago.silvestre@ua.pt>
2024-06-08 15:01:31 +0100

    add lock for database

--- api/src/db.py
+++ api/src/db.py
@@ -1,65 +1,70 @@
 import sqlite3
 from typing import List, Any
 import logging
+import threading
 
 
 class DB:
     def __init__(self, db_name: str):
         self.db_name = db_name
+        self.lock = threading.Lock()
 
     def _connect(self):
         return sqlite3.connect(self.db_name)
     
     def runGetQuery(self, query: str, args: List[Any] = []) -> List[tuple]:
-        conn = self._connect()
-        cursor = conn.cursor() 
+        with self.lock:
+            conn = self._connect()
+            cursor = conn.cursor() 
 
-        try:
-            cursor.execute(query, args)
-            results = cursor.fetchall()
-        except sqlite3.Error as e:
-            logging.error(f"An error ocurred: {e}")
-            return []
-        
-        finally:
-            cursor.close()
-            conn.close()
+            try:
+                cursor.execute(query, args)
+                results = cursor.fetchall()
+            except sqlite3.Error as e:
+                logging.error(f"An error ocurred: {e}")
+                return []
+            
+            finally:
+                cursor.close()
+                conn.close()
 
-        return results
+            return results
     
     def runUpdateQuery(self, query: str, args: List[Any] = []) -> int:
-        conn = self._connect()
-        cursor = conn.cursor()
-        try:
-            cursor.execute(query, args)
-            conn.commit()
-            affected_rows = cursor.rowcount
-        except sqlite3.Error as e:
-            logging.error(f"An error ocurred: {e}, for query '{query}' with args {str(args)}")
-            conn.rollback()
-            affected_rows = 0
+        with self.lock:
+            conn = self._connect()
+            cursor = conn.cursor()
+            try:
+                cursor.execute(query, args)
+                conn.commit()
+                affected_rows = cursor.rowcount
+            except sqlite3.Error as e:
+                logging.error(f"An error ocurred: {e}, for query '{query}' with args {str(args)}")
+                conn.rollback()
+                affected_rows = 0
 
-        finally:
-            cursor.close()
-            conn.close()
+            finally:
+                cursor.close()
+                conn.close()
 
-        return affected_rows
+            return affected_rows
     
     
     
     def runScript(self, script: str):
-        conn = self._connect()
-        cursor = conn.cursor()
+        with self.lock:
+            conn = self._connect()
+            cursor = conn.cursor()
 
-        try:
-            cursor.executescript(script)
-            conn.commit()
-        except sqlite3.Error as e:
-            logging.error(f"An error occurred: {e}")
-            conn.rollback()
-        finally:
-            cursor.close()
-            conn.close()
+            try:
+                cursor.executescript(script)
+                conn.commit()
+            except sqlite3.Error as e:
+                logging.error(f"An error occurred: {e}")
+                conn.rollback()
+            finally:
+                cursor.close()
+                conn.close()
         
 
 _instance = None
